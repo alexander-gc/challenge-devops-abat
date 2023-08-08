4d256b924ae3cc26661c2041b0fd04f7b024a621
alexander-gc <alexandergcca@gmail.com>
2023-08-08 01:45:54 -0600

    Changes in scripts

--- k8s/scripts/start-depl.sh
+++ k8s/scripts/start-depl.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# ===================== SETTING UP GIT  =========================== #
+USERNAME_GITHUB=$1
+EMAIL_GITHUB=$2
+
+if [ ! -n "$USERNAME_GITHUB" -o ! -n "$EMAIL_GITHUB" ]; then
+    echo "Username and email of Github must be provided"
+    exit 2
+fi
+
+git config --global user.name "$USERNAME_GITHUB"
+git config --global user.email "$EMAIL_GITHUB"
+
+# ===================== START UP KUBERNETES DEPLOYMENTS =========================== #
 K8S_HOME="$HOME/challenge-devops-abat/k8s"
 
 echo "==> Pulling nestjs Image"
